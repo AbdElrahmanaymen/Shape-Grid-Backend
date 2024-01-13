dcd1839f85afb726d614384e74cbf43f6ef8d51e
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-01-14 07:50:22 +0800

    Update settings.py

--- alphv_backend/settings.py
+++ alphv_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +138,6 @@ CHANNEL_LAYERS = {
         "CONFIG": {"hosts": [("127.0.0.1", 6379)]},
     }
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
