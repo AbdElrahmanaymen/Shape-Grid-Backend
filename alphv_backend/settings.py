157d80e9937e63323cf9ec474eb6be7736148abd
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-01-14 09:13:51 +0800

    Update settings.py

--- alphv_backend/settings.py
+++ alphv_backend/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = [".herokuapp.com", "https://shapegrid-1b327639b860.herokuapp.com
 # Application definition
 
 INSTALLED_APPS = [
-    "corsheaders",
     "daphne",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'channels.middleware.ChannelsMiddleware',
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
     # 'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
