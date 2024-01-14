35490e0c5cf0394bb5bcec4718030826930717b6
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-01-14 09:23:25 +0800

    Update asgi.py

--- alphv_backend/asgi.py
+++ alphv_backend/asgi.py
@@ -12,15 +12,20 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-import django
 from django.core.asgi import get_asgi_application
 from base.routing import websocket_urlpatterns
-from channels.routing import get_default_application
 
 import base.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alphv_backend.settings")
 
-django.setup()
+django_asgi_app = get_asgi_application()
 
-application = get_default_application()
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
